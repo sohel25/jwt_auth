9e9c4d7938c6aa5434b006794420e8f7ea18c214
sohel khan <sokhan@bestpeers.com>
2021-03-22 19:47:12 +0530

    user registration and genrate jwt token

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const user = require('./models/users'); 
+const user = require('./models/users');
+var bodyParser = require("body-parser");
+var jsonParsor = bodyParser.json(); 
+var crypto = require ('crypto');
+var key ="password";
+var algo ="aes256";
+const jwt = require("jsonwebtoken");
+jwtkey="jwt";
+
 mongoose.connect('mongodb+srv://new_user:dYzgb7ZDO3BPFsZU@cluster0.4swmy.mongodb.net/jwtdb?retryWrites=true&w=majority',{
     useNewUrlParser:true,
     useUnifiedTopology:true
@@ -9,6 +17,31 @@ mongoose.connect('mongodb+srv://new_user:dYzgb7ZDO3BPFsZU@cluster0.4swmy.mongodb
     console.log("Connected");
 })
 
+app.post('/register',jsonParsor,(req,res)=>{
+    var cipher = crypto.createCipher(algo,key);
+    var encrypted= cipher.update(req.body.password,'utf-8','hex')
+    +cipher.final('hex');
+    console.log(encrypted);
+    
+    console.log(req.body);
+    
+    const data = new user({
+        _id:mongoose.Types.ObjectId(),
+    name:req.body.name,
+    email:req.body.email,
+    address:req.body.address,
+    password:encrypted
+    });
+    data.save().then((result)=>{
+        jwt.sign({result},jwtkey,{expiresIn:'3000s'},(err,token)=>{
+            res.status(201).json({token})
+        })
+        // res.status(200).json(result);
+    }).catch((err)=>{
+        console.log(err);
+    });
+    // res.send("hello");
+} )
 
 app.get("/",(req,res)=>{
     res.end("hello");
@@ -16,6 +49,6 @@ app.get("/",(req,res)=>{
 
 
 })
-app.listen(3000,()=>{
-    console.log("server at 3000");
+app.listen(4000,()=>{
+    console.log("server at 4000");
 })
--- models/users.js
+++ models/users.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 let userSchema = new mongoose.Schema({
-    _id:mongoose.Schema.Types.ObjectId,
+    
     name:String,
     email:String,
     address:String,
